50886d827a1a2eed53c9f5a2f60f9a1b1d0620fb
shibale <shibale9712@gmail.com>
2017-05-29 22:18:56 +0800

    選擇新英靈後會把當前的英靈設定回歸預設值。

--- client/servant/selectUseServant.js
+++ client/servant/selectUseServant.js
@@ -16,7 +16,12 @@ Template.selectUseServant.helpers({
 function selectHandler(selectedServantData, templateInstance) {
   selectedServantData.lastUseTime = Date.now();
   const useServantData = templateInstance.data.model;
+  useServantData.nickname = '';
   useServantData.servantId = selectedServantData.id;
+  useServantData.atk = 0;
+  useServantData.useWeaponIndex = 0;
+  useServantData.weaponLevel = 1;
+  useServantData.currentNp = 0;
   FlowRouter.go('/fgo-simulator/editUseServant/' + useServantData.id);
 }
 Template.selectUseServant.onCreated(function() {
